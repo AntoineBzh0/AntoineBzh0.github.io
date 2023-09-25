8a83b3c7f7e77efd91913230b53ec20a9c59b148
AntoineBzh0 <144608176+AntoineBzh0@users.noreply.github.com>
2023-09-25 18:47:33 +0200

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -1,33 +1,16 @@
 var numeric = [
-	["3/2/23","Antoine & Kev","MrjLaine & Flo","MrjRie"], //same
-	["3/18/23","Antoine & MrjRie","Kev & Flo","MrjLaine"],
-	["3/25/23","Kev & MrjLaine","MrjRie & Antoine","Flo"],
-	["4/1/23","MrjRie & Antoine","MrjLaine & Flo","Kev"],
-	["4/8/23","MrjLaine & Flo","MrjRie & Kev","Antoine"],
-	["4/15/23","Antoine & Kev","MrjLaine & Flo","MrjRie"],
-	["4/22/23","MrjRie & Flo","Antoine & Kev","MrjLaine"],
-	["4/29/23","Antoine & Kev","MrjRie & MrjLaine","Flo"],
-	["5/6/23","MrjRie & MrjLaine","Antoine & Flo","Kev"],
-	["5/13/23","Kev & Flo","MrjRie & MrjLaine","Antoine"],
-	["5/20/23","Antoine & MrjLaine","Kev & Flo","MrjRie"],
-	["5/27/23","Kev & Flo","Antoine & MrjRie","MrjLaine"],
-	["6/3/23","Antoine & MrjRie","Kev & MrjLaine","Flo"],
-	["6/10/23","MrjLaine & Flo","Antoine & MrjRie","Kev"],
-	["6/17/23","MrjRie & Kev","MrjLaine & Flo","Antoine"],
-	["6/24/23","MrjLaine & Flo","Antoine & Kev","MrjRie"],
-	["7/1/23","Antoine & Kev","MrjRie & Flo","MrjLaine"],
-	["7/8/23","MrjRie & MrjLaine","Antoine & Kev","Flo"],
-	["7/15/23","Antoine & Flo","MrjRie & MrjLaine","Kev"],
-	["7/22/23","MrjRie & MrjLaine","Kev & Flo","Antoine"],
-	["7/29/23","Kev & Flo","Antoine & MrjLaine","MrjRie"], 
-	["8/5/23","Antoine & MrjRie","Kev & Flo","MrjLaine"],
-	["8/12/23","Kev & MrjLaine","Antoine & MrjRie","Flo"],
-	["8/19/23","Antoine & MrjRie","MrjLaine & Flo","Kev"],
-	["8/26/23","MrjLaine & Flo","MrjRie & Kev","Antoine"],
-	["9/2/23","Antoine & Kev","MrjLaine & Flo","MrjRie"], //same
-	["9/9/23","This is","the end...","Adele"],
-	["9/16/23","One more","night!!!","..Daft Punk"],
-	["9/23/23","One more","night!!!","..Daft Punk"]
+	["9/29/23","Antoine & Kev","Heloise & Xav",""], //same
+	["10/6/23","Antoine & Xav","Keke & Heloise",""],
+	["10/13/23","Heloise et Xav","Antoine & Keke",""], 
+	["10/20/23","Keke & Heloise","Antoine & Xav",""],//same
+	["10/27/23","Antoine & Kev","Heloise & Xav",""], //same
+	["11/4/23","Antoine & Xav","Keke & Heloise",""],
+	["11/11/23","Heloise et Xav","Antoine & Keke",""], 
+	["11/18/23","Keke & Heloise","Antoine & Xav",""],//same
+	["11/25/23","Antoine & Kev","Heloise & Xav",""], //same
+	["12/01/23","Antoine & Xav","Keke & Heloise",""],
+	["12/08/23","Heloise et Xav","Antoine & Keke",""], 
+	["12/15/23","Keke & Heloise","Antoine & Xav",""] //same
 ];
 
 var i = 0;
